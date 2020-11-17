d273dcdc27788d814e3cd7b2bb2d4eae919402e4
Jonathan Hunt <jonathan.james.hunt@outlook.com>
2020-11-17 20:06:37 +0000

    added db variables

---
+++ CN_Project/Terraform/RDS/variables.tf
@@ -0,0 +1,9 @@
+variable "DB_USER" {
+  description = "name of user"
+  default     = "JonathanQA"
+}
+
+variable "DB_PASSWORD" {
+  description = "password for db"
+  default     = "Password1234!"
+}
