bfeff65c7d5337947fdcd79167eafb1563dcb154
Jonathan Hunt <jonathan.james.hunt@outlook.com>
2020-11-18 11:24:05 +0000

    removed duplicate folder

--- CN_Project/Terraform/SecurityGroup/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "aws_project_sg" {
-  value = aws_security_group.aws_project_sg.id
-}
--- CN_Project/Terraform/VPC/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "vpc_id" {
-  value = aws_vpc.project_vpc.id
-}
-
-output "project_sn_id" {
-  value = aws_subnet.project_sn.id
-}
