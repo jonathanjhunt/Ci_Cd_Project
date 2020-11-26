70dedd2b52c14bd874569812b02c444db178326d
Jonathan Hunt <70526178+jonathanjhunt@users.noreply.github.com>
2020-11-26 14:07:10 +0000

    Update variables.tf

--- CN_Project/Terraform/RDS/variables.tf
+++ CN_Project/Terraform/RDS/variables.tf
@@ -1,11 +1,10 @@
 variable "DB_USER" {
   description = "name of user"
-  default     = "JonathanQA"
 }
 
 variable "DB_PASSWORD" {
   description = "password for db"
-  default     = "Password1234!"
+  
 }
 variable "subnet1_id" {
   description = "to be replaced"
